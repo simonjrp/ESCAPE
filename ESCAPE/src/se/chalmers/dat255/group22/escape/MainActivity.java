09fb7f489e06258869c445e2b10a54e7e2da90e0
AdamBook <cadambook@gmail.com>
2013-09-17 11:51:13 +0200

    Added method for switching fragments.

--- ESCAPE/src/se/chalmers/dat255/group22/escape/MainActivity.java
+++ ESCAPE/src/se/chalmers/dat255/group22/escape/MainActivity.java
@@ -1,9 +1,13 @@
 package se.chalmers.dat255.group22.escape;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -30,8 +34,33 @@ public class MainActivity extends Activity {
 		// Sets which items the ListView should show
 		mDrawerList.setAdapter(new ArrayAdapter<String>(this,
 				R.layout.drawer_list_item, fragmentTitles));
+
+		mDrawerList.setOnItemClickListener(new ListView.OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				selectItem(position);
+
+			}
+
+		});
 	}
 
+	public void selectItem(int position){
+		Fragment fragment = new TestFragment();
+		Bundle args = new Bundle();
+		args.putString("TITLE", fragmentTitles[position]);
+		fragment.setArguments(args);
+		
+		FragmentManager fragmentManager = getFragmentManager();
+		fragmentManager.beginTransaction()
+			.replace(R.id.content_frame,  fragment).commit();
+		mDrawerList.setItemChecked(position, true);
+		setTitle(fragmentTitles[position]);
+		mDrawerLayout.closeDrawer(mDrawerList);
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
