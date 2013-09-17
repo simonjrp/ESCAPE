25b46418ffb0d11452f5dcbb1030cf6af1a71533
AdamBook <cadambook@gmail.com>
2013-09-17 10:35:04 +0200

    Added TestFragment.java

--- ESCAPE/src/se/chalmers/dat255/group22/escape/TestFragment.java
+++ ESCAPE/src/se/chalmers/dat255/group22/escape/TestFragment.java
@@ -1,5 +1,18 @@
 package se.chalmers.dat255.group22.escape;
 
-public class TestFragment {
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+public class TestFragment extends Fragment{
+
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		
+		View v = inflater.inflate(R.layout.test_fragment, container, false);
+		
+		return v;
+		
+	}
 }
