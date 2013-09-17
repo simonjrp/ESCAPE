36f12f13a1b08118244e947c08af4ac2d76f6d2f
simonjrp <simon.jrp@gmail.com>
2013-09-17 11:15:05 +0200

    Modified MainActivity to make use of the drawer layout

--- ESCAPE/src/se/chalmers/dat255/group22/escape/MainActivity.java
+++ ESCAPE/src/se/chalmers/dat255/group22/escape/MainActivity.java
@@ -1,18 +1,35 @@
 package se.chalmers.dat255.group22.escape;
 
-/**
- * Hej hopp!
- */
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
+
+	private DrawerLayout mDrawerLayout;
+	private ListView mDrawerList;
+
+	private CharSequence mTitle;
+	private String[] fragmentTitles;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		// Saving title of application for later use
+		mTitle = getTitle();
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+		fragmentTitles = getResources().getStringArray(R.array.fragments_array);
+
+		// Sets which items the ListView should show
+		mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+				R.layout.drawer_list_item, fragmentTitles));
 	}
 
 	@Override
--- ESCAPE/src/se/chalmers/dat255/group22/escape/TestFragment.java
+++ ESCAPE/src/se/chalmers/dat255/group22/escape/TestFragment.java
@@ -6,13 +6,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class TestFragment extends Fragment{
+public class TestFragment extends Fragment {
 
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
 		View v = inflater.inflate(R.layout.test_fragment, container, false);
-		
+
 		return v;
-		
+
 	}
 }
