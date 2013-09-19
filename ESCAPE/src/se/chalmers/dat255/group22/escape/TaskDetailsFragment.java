9d9ffdd80d42cc53da3cf5b0710f8122b7c39150
simonjrp <simon.jrp@gmail.com>
2013-09-19 18:50:57 +0200

    Spinner in task details fragment now shows some test categories

--- ESCAPE/src/se/chalmers/dat255/group22/escape/TaskDetailsFragment.java
+++ ESCAPE/src/se/chalmers/dat255/group22/escape/TaskDetailsFragment.java
@@ -1,5 +1,7 @@
 package se.chalmers.dat255.group22.escape;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.zip.Inflater;
 
 import android.app.Fragment;
@@ -7,13 +9,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class TaskDetailsFragment extends Fragment {
 
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
 		View v = inflater.inflate(R.layout.task_details, container, false);
-		
 		return v;
 	}
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+       super.onActivityCreated(savedInstanceState);
+        Spinner catSpinner = (Spinner)getActivity().findViewById(R.id.task_categories);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.test_categories, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        catSpinner.setAdapter(adapter);
+    }
 	
 }
