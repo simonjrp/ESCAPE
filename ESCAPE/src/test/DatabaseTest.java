c8e343170051ac960a00de4130a2415ff4ef22d8
Anno <anno.tao@gmail.com>
2013-09-23 16:38:15 +0200

    Added test class

---
+++ ESCAPE/src/test/DatabaseTest.java
@@ -0,0 +1,15 @@
+package test;
+
+import junit.framework.TestCase;
+
+public class DatabaseTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+	
+}
