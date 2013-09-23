556549faeca7b2fd78b2d90b17be3a2d869a2956
Anno <anno.tao@gmail.com>
2013-09-23 17:12:56 +0200

    Added support for the test to open a new database so that it doesn't use any existing database

--- ESCAPE_Test/src/se/chalmers/dat255/group22/escape/test/TestDatabase.java
+++ ESCAPE_Test/src/se/chalmers/dat255/group22/escape/test/TestDatabase.java
@@ -1,16 +1,28 @@
 package se.chalmers.dat255.group22.escape.test;
 
+import se.chalmers.dat255.group22.escape.DBHandler;
 import android.test.AndroidTestCase;
+import android.test.RenamingDelegatingContext;
 
 public class TestDatabase extends AndroidTestCase {
 
+	private DBHandler db;
+	
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
+		RenamingDelegatingContext context 
+        = new RenamingDelegatingContext(getContext(), "test_");
+        db = new DBHandler(context);
 	}
+	
+	public void testAddEntry(){
+        // Here i have my new database wich is not connected to the standard database of the App
+    }
 
 	@Override
 	protected void tearDown() throws Exception {
+		db.close();
 		super.tearDown();
 	}
 
