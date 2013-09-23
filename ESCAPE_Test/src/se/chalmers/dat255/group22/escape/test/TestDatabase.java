f265770c675efde8681d8bf7101630906753a6a6
Anno <anno.tao@gmail.com>
2013-09-23 17:03:05 +0200

    Added test project and removed previous test attempt...

---
+++ ESCAPE_Test/src/se/chalmers/dat255/group22/escape/test/TestDatabase.java
@@ -0,0 +1,17 @@
+package se.chalmers.dat255.group22.escape.test;
+
+import android.test.AndroidTestCase;
+
+public class TestDatabase extends AndroidTestCase {
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
