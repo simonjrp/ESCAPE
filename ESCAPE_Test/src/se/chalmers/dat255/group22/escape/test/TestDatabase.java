92e206403373062c2ca495d3a4dbf0cba2867a0b
Johanna Eriksson <e.johanne.eriksson@gmail.com>
2013-09-24 08:42:39 +0200

    Added test methods

--- ESCAPE_Test/src/se/chalmers/dat255/group22/escape/test/TestDatabase.java
+++ ESCAPE_Test/src/se/chalmers/dat255/group22/escape/test/TestDatabase.java
@@ -1,16 +1,19 @@
 package se.chalmers.dat255.group22.escape.test;
 
+import java.sql.Date;
 import java.util.List;
 
+import se.chalmers.dat255.group22.escape.Category;
 import se.chalmers.dat255.group22.escape.DBHandler;
 import se.chalmers.dat255.group22.escape.ListObject;
+import se.chalmers.dat255.group22.escape.Place;
+import se.chalmers.dat255.group22.escape.Time;
 import android.test.AndroidTestCase;
 import android.test.RenamingDelegatingContext;
 
 public class TestDatabase extends AndroidTestCase {
 
 	private DBHandler db;
-	
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
@@ -19,6 +22,7 @@ public class TestDatabase extends AndroidTestCase {
         db = new DBHandler(context);
 	}
 	
+	// ListObject tests
 	public void testAddEntry(){
 		ListObject lo = new ListObject(1, "Test");
 		
@@ -45,6 +49,89 @@ public class TestDatabase extends AndroidTestCase {
 		assertEquals(true, list.isEmpty());
 		
 	}
+	
+	// Categories tests
+	public void testAddCategory(){
+		Category category = new Category("Test1", "Color1", "Color");
+		
+        db.addCategory(category);
+        List<Category> list = db.getAllCategories();
+        assertEquals(category, list.get(0));
+    }
+	
+	public void testEditCategory(){ 
+		Category category = new Category("Test1", "Color1", "Color");
+        db.addCategory(category);
+		List<Category> list = db.getAllCategories();
+        category.setName("Test2");
+        db.updateCategory(category, "Test1");
+        list = db.getAllCategories();
+        assertEquals(category.getName(), list.get(0).getName());
+    }
+
+	public void testRemoveCategory(){
+		List<Category> list = db.getAllCategories();
+		Category category = list.get(0);
+		db.deleteCategory(category);
+		assertEquals(true, list.isEmpty());
+	}
+	
+	//Places tests
+	public void testAddPlace(){
+		Place place = new Place(1, "Test");
+		
+        db.addPlace(place);
+        List<Place> list = db.getAllPlaces();
+        assertEquals(place, list.get(0));
+    }
+	
+	public void testEditPlace(){ 
+		List<Place> list = db.getAllPlaces();
+        Place place = list.get(0);
+        place.setName("Test2");
+        
+        db.updatePlaces(place);
+        list = db.getAllPlaces();
+        assertEquals(place.getName(), list.get(0).getName());
+    }
+
+	
+	public void testRemovePlace(){
+		List<Place> list = db.getAllPlaces();
+		Place place = list.get(0);
+		db.deletePlace(place);
+		assertEquals(true, list.isEmpty());
+	}
+	
+	//Time tests
+	public void testAddTime(){
+		Date date1 = new Date(1l);
+		Date date2 = new Date(2l);
+		Time time = new Time(1, date1, date2);
+		
+        db.addTime(time);
+        List<Time> list = db.getAllTimes();
+        assertEquals(time, list.get(0));
+    }
+	
+	public void testEditTime(){ 
+		Date date3 = new Date(3l);
+		List<Time> list = db.getAllTimes();
+        Time time = list.get(0);
+        time.setEndDate(date3);
+        db.updateTimes(time);
+        list = db.getAllTimes();
+        assertEquals(time.getEndDate(), list.get(0).getEndDate());
+    }
+
+	
+	public void testRemoveTime(){
+		List<Time> list = db.getAllTimes();
+		Time time = list.get(0);
+		db.deleteTime(time);
+		assertEquals(true, list.isEmpty());
+	}
+		
 	@Override
 	protected void tearDown() throws Exception {
 		db.close();
