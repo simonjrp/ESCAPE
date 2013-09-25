6bf23b85843c12543187fbcb4f9733b2de8b7c23
simonjrp <simon.jrp@gmail.com>
2013-09-25 19:23:05 +0200

    Added test for MainActivity (might not be complete)

---
+++ ESCAPE_Test/src/se/chalmers/dat255/group22/escape/test/TestMainActivity.java
@@ -0,0 +1,41 @@
+package se.chalmers.dat255.group22.escape.test;
+
+import se.chalmers.dat255.group22.escape.MainActivity;
+import se.chalmers.dat255.group22.escape.SectionsPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.test.ActivityInstrumentationTestCase2;
+
+public class TestMainActivity extends
+		ActivityInstrumentationTestCase2<MainActivity> {
+
+	private MainActivity mActivity;
+	private ViewPager viewPager;
+	private SectionsPagerAdapter pagerAdapter;
+	public static final int ADAPTER_COUNT = 3;
+
+	public TestMainActivity() {
+		super("se.chalmers.dat255.group22.escape.MainActivity",
+				MainActivity.class);
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+
+		// disable touch mode on the device/vm
+		setActivityInitialTouchMode(false);
+
+		// get basic objects needed in the test
+		mActivity = getActivity();
+		viewPager = (ViewPager) mActivity
+				.findViewById(se.chalmers.dat255.group22.escape.R.id.view_pager);
+		pagerAdapter = (SectionsPagerAdapter) viewPager.getAdapter();
+	}
+
+	public void testPreConditions() {
+		assertTrue(viewPager != null);
+		assertTrue(pagerAdapter != null);
+		assertEquals(pagerAdapter.getCount(), ADAPTER_COUNT);
+	}
+
+}
