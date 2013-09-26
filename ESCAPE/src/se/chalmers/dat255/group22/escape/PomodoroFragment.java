15d053d268004d00592d23816c8fd5b80012bc89
AdamBook <cadambook@gmail.com>
2013-09-26 19:53:57 +0200

    Added an initial timer

--- ESCAPE/src/se/chalmers/dat255/group22/escape/PomodoroFragment.java
+++ ESCAPE/src/se/chalmers/dat255/group22/escape/PomodoroFragment.java
@@ -1,20 +1,66 @@
 package se.chalmers.dat255.group22.escape;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class PomodoroFragment extends Fragment {
+public class PomodoroFragment extends Fragment implements OnClickListener {
+
+	private CountDownTimer countDownTimer;
+	private boolean timerHasStarted = false;
+	private Button startB;
+	public TextView text;
+	private final long startTime = 5 * 1000;
+	private final long interval = 1 * 1000;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 
 		View v = inflater.inflate(R.layout.pomodoro_fragment, container, false);
-		
+
+		startB = (Button) v.findViewById(R.id.pomodoro_button);
+		startB.setOnClickListener(this);
+		text = (TextView) v.findViewById(R.id.pomodoro_timer);
+		countDownTimer = new MyCountDownTimer(startTime, interval);
+		text.setText(text.getText() + String.valueOf(startTime / 1000));
+
 		return v;
 	}
+
+	@Override
+	public void onClick(View v) {
+		if (!timerHasStarted) {
+			countDownTimer.start();
+			timerHasStarted = true;
+			startB.setText("STOP");
+		} else {
+			countDownTimer.cancel();
+			timerHasStarted = false;
+			startB.setText("RESTART");
+		}
+	}
+
+	public class MyCountDownTimer extends CountDownTimer {
+		public MyCountDownTimer(long startTime, long interval) {
+			super(startTime, interval);
+		}
+
+		@Override
+		public void onFinish() {
+			text.setText("Time's up!");
+		}
+
+		@Override
+		public void onTick(long millisUntilFinished) {
+			text.setText("" + millisUntilFinished / 1000);
+		}
+	}
+
 }
