bd01e7353acd4d0625ec7675534baba35370bfe7
Carl Jansson <carjan91@gmail.com>
2013-09-26 19:43:39 +0200

    Tried to sort evnents into event list. Makes app crash

--- ESCAPE/src/se/chalmers/dat255/group22/escape/ListFragment/ExpandableEventListFragment.java
+++ ESCAPE/src/se/chalmers/dat255/group22/escape/ListFragment/ExpandableEventListFragment.java
@@ -1,12 +1,16 @@
 package se.chalmers.dat255.group22.escape.ListFragment;
 
+import java.sql.Date;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.List;
 
 import se.chalmers.dat255.group22.escape.DBHandler;
 import se.chalmers.dat255.group22.escape.ListObject;
 import se.chalmers.dat255.group22.escape.R;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -58,26 +62,30 @@ public class ExpandableEventListFragment extends Fragment {
 	@Override
 	public void onResume() {
 		super.onResume();
-		DBHandler dbHandler = new DBHandler(getActivity());
+
+        DBHandler dbHandler = new DBHandler(getActivity());
 		List<ListObject> listObjects = dbHandler.getAllListObjects();
 
-        for(ListObject lo:listObjects) {
-            // we only want evens in this fragment (objects with a set time)
-            if(lo.getTime() != null) {
-
+		for (ListObject lo : listObjects) {
+			// we only want evens in this fragment (objects with a set time)
+			if (dbHandler.getTime(lo).getStartDate() != null) {
+				addListObject(lo);
+			} else {
+                //dbHandler.addListObjectsWithTime(lo, new Time(1, new Date(System.currentTimeMillis()), new Date(System.currentTimeMillis()+1)));
+                addListObject(lo);
             }
-        }
+		}
 
 		// TODO Ugly code for 'updating' the expandable list view. Without this,
 		// if a category list is expanded before adding a new activity, you
 		// have to collapse and expand that category list to be able to see the
 		// newly added item.
-        /*
-        Look into implementing the following methods in CustomExpandableList
-        public void notifyDataSetChanged ()
-        public void registerDataSetObserver (DataSetObserver observer)
-        public void unregisterDataSetObserver (DataSetObserver observer)
-         */
+		/*
+		 * Look into implementing the following methods in CustomExpandableListAdapter
+		 * public void notifyDataSetChanged () public void
+		 * registerDataSetObserver (DataSetObserver observer) public void
+		 * unregisterDataSetObserver (DataSetObserver observer)
+		 */
 
 		ExpandableListView expLv = (ExpandableListView) getActivity()
 				.findViewById(R.id.expEventList);
@@ -87,13 +95,13 @@ public class ExpandableEventListFragment extends Fragment {
 				expLv.expandGroup(i);
 			}
 		}
-        expLv.expandGroup(0, true);
+		expLv.expandGroup(0, true);
 
 	}
 
-	/*
-	 * Preparing the dummy list data
-	 */
+    /**
+     * Prepare the dummy list data
+     */
 	private void getListData() {
 
 		eventDataMap = new HashMap<String, List<ListObject>>();
@@ -113,7 +121,28 @@ public class ExpandableEventListFragment extends Fragment {
 		eventDataMap.put(getResources().getString(R.string.thisWeekLabel),
 				thisWeekEventList);
 
+	}
 
+	/**
+	 * Add a list object to the expandable list. Object should automatically be
+	 * placed where it should be.
+	 * 
+	 * @param listObject
+	 *            the listObject to add
+	 */
+	public void addListObject(ListObject listObject) {
+
+		// Get a calendar with current system time
+		/*Date theDate = dbHandler.getTime(listObject).getStartDate();
+
+		//TODO Check if this works and maybe add additional functionality!
+		if (isToday(theDate)) {
+			addListObjectToday(listObject);
+		} else if (isTomorrow(theDate)) {
+			addListObjectTomorrow(listObject);
+		} else {
+			addListObjectThisWeek(listObject);
+		}*/
 	}
 
 	/**
@@ -124,19 +153,20 @@ public class ExpandableEventListFragment extends Fragment {
 	 */
 	public void addListObjectToday(ListObject listObject) {
 
-        boolean alreadyExists = false;
-
-        // TODO ugly, temporary code so that the same listobject doesn't get
-        // added multiple times
-        for (ListObject lo : todayEventList) {
-            if (listObject.getId() == lo.getId() ){
-                alreadyExists = true;
-            }
-        }
-
-        if (!alreadyExists) {
-            todayEventList.add(listObject);
-        }
+		/*
+		 * boolean alreadyExists = false;
+		 * 
+		 * // TODO ugly, temporary code so that the same listobject doesn't get
+		 * // added multiple times
+		 * for (ListObject lo : todayEventList) {
+		 * if
+		 * (listObject.getId() == lo.getId() ){
+		 * alreadyExists = true;
+		 * } }
+		 */
+		if (!todayEventList.contains(listObject)) {
+			todayEventList.add(listObject);
+		}
 	}
 
 	/**
@@ -146,7 +176,9 @@ public class ExpandableEventListFragment extends Fragment {
 	 *            the listObject to add
 	 */
 	public void addListObjectTomorrow(ListObject listObject) {
-		tomorrowEventList.add(listObject);
+		if (!tomorrowEventList.contains(listObject)) {
+			tomorrowEventList.add(listObject);
+		}
 	}
 
 	/**
@@ -155,9 +187,51 @@ public class ExpandableEventListFragment extends Fragment {
 	 * @param listObject
 	 *            the listObject to add
 	 */
-
 	public void addListObjectThisWeek(ListObject listObject) {
-		thisWeekEventList.add(listObject);
+		if (!thisWeekEventList.contains(listObject)) {
+			thisWeekEventList.add(listObject);
+		}
 	}
 
+	/**
+	 * Method to check if a date is today
+	 * 
+	 * @param theDate
+	 *            the date to see if it is today
+	 * @return true if it is today
+	 */
+	private boolean isToday(Date theDate) {
+		// Get a calendar with the events start time
+		GregorianCalendar theCalendar = new GregorianCalendar();
+		theCalendar.setTime(theDate);
+        // Get a calendar with current system time and set it to day after today
+        Calendar tomorrow = Calendar.getInstance();
+        tomorrow.roll(Calendar.DAY_OF_YEAR, true);
+        tomorrow.set(Calendar.HOUR_OF_DAY, 0);
+
+		return theCalendar.before(tomorrow);
+	}
+
+	/**
+	 * Method to check if a date is tomorrow
+	 * 
+	 * @param theDate
+	 *            the date to see if it is tomorrow
+	 * @return true of it is tomorrow
+	 */
+	private boolean isTomorrow(Date theDate) {
+		// Get a calendar with the events start time
+		GregorianCalendar theCalendar = new GregorianCalendar();
+		theCalendar.setTime(theDate);
+		// Get a calendar with current system time and set it to day after tomorrow
+		Calendar dayAfterTomorrow = Calendar.getInstance();
+        dayAfterTomorrow.set(Calendar.HOUR_OF_DAY, 0);
+        dayAfterTomorrow.roll(Calendar.DAY_OF_YEAR, true);
+        if (theCalendar.before(dayAfterTomorrow)) {
+            return false;
+        }
+        dayAfterTomorrow.roll(Calendar.DAY_OF_YEAR, true);
+
+		return theCalendar.before(dayAfterTomorrow);
+	}
 }
