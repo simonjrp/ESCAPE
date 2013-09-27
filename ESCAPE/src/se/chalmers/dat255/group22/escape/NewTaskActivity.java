116cd93eaba01f567a985f325b96086ee053cbdd
simonjrp <simon.jrp@gmail.com>
2013-09-27 15:25:15 +0200

    It's now possible to actually add a reminder, but only with predefined time and date values

--- ESCAPE/src/se/chalmers/dat255/group22/escape/NewTaskActivity.java
+++ ESCAPE/src/se/chalmers/dat255/group22/escape/NewTaskActivity.java
@@ -1,6 +1,7 @@
 package se.chalmers.dat255.group22.escape;
 
 import java.sql.Date;
+import java.util.Calendar;
 
 import android.app.Activity;
 import android.app.FragmentManager;
@@ -18,6 +19,7 @@ import android.widget.Toast;
 public class NewTaskActivity extends Activity {
 
 	static final long DAY_IN_MILLIS = 1000 * 60 * 60 * 24;
+	private boolean remindMeClicked;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -45,11 +47,11 @@ public class NewTaskActivity extends Activity {
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		// Make home button in actionbar work like pressing on backbutton
-			case android.R.id.home :
-				onBackPressed();
-				return true;
-			default :
-				return super.onOptionsItemSelected(item);
+		case android.R.id.home:
+			onBackPressed();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
 		}
 	}
 
@@ -99,6 +101,59 @@ public class NewTaskActivity extends Activity {
 		}
 
 		// Time Alarm
+		// TODO It's possible to select a time for the reminder that might've passed already
+		TimeAlarm timeAlarm = null;
+		if (remindMeClicked) {
+			Spinner reminderDateSpinner = (Spinner) findViewById(R.id.reminderDateSpinner);
+			Spinner reminderTimeSpinner = (Spinner) findViewById(R.id.reminderTimeSpinner);
+			Calendar calendar = Calendar.getInstance();
+			calendar.setTimeInMillis(System.currentTimeMillis());
+			switch (reminderDateSpinner.getSelectedItemPosition()) {
+			case 0:
+				// date already correct
+				break;
+			case 1:
+				calendar.set(Calendar.DAY_OF_MONTH,
+						calendar.get(Calendar.DAY_OF_MONTH) + 1);
+				break;
+			case 2:
+				calendar.set(Calendar.DAY_OF_MONTH,
+						calendar.get(Calendar.DAY_OF_MONTH) + 2);
+				break;
+			default:
+				// TODO do nothing
+				break;
+			}
+
+			switch (reminderTimeSpinner.getSelectedItemPosition()) {
+			case 0:
+				calendar.set(Calendar.HOUR_OF_DAY, 9);
+				calendar.set(Calendar.MINUTE, 0);
+				calendar.set(Calendar.SECOND, 0);
+				break;
+			case 1:
+				calendar.set(Calendar.HOUR_OF_DAY, 13);
+				calendar.set(Calendar.MINUTE, 0);
+				calendar.set(Calendar.SECOND, 0);
+				break;
+			case 2:
+				calendar.set(Calendar.HOUR_OF_DAY, 17);
+				calendar.set(Calendar.MINUTE, 0);
+				calendar.set(Calendar.SECOND, 0);
+				break;
+			case 3:
+				calendar.set(Calendar.HOUR_OF_DAY, 20);
+				calendar.set(Calendar.MINUTE, 0);
+				calendar.set(Calendar.SECOND, 0);
+				break;
+			default:
+				// TODO do nothing
+				break;
+			}
+
+			Date date = new Date(calendar.getTimeInMillis());
+			timeAlarm = new TimeAlarm(0, date);
+		}
 
 		// GPS Alarm
 
@@ -117,6 +172,8 @@ public class NewTaskActivity extends Activity {
 
 			time = new Time(1, startDate, endDate);
 
+			DBHandler dbHandler = new DBHandler(this);
+
 			ListObject lo = new ListObject(1, name);
 			if (comment.trim().length() != 0)
 				lo.setComment(comment);
@@ -124,9 +181,21 @@ public class NewTaskActivity extends Activity {
 			lo.addToCategory(newCategory);
 			lo.setPlace(place);
 			lo.setTime(time);
+			
+			// add timealarm and create notification
+			if(remindMeClicked) {
+				NotificationHandler nf = new NotificationHandler(this.getParent());
+				
+				long idLo = dbHandler.addListObject(lo);
 
-			DBHandler dbHandler = new DBHandler(this);
-			dbHandler.addListObject(lo);
+				long idAlarm = dbHandler.addTimeAlarm(timeAlarm);
+
+				dbHandler.addListObjectWithTimeAlarm(dbHandler.getListObject(idLo),
+						dbHandler.getTimeAlarm(idAlarm));
+				
+				nf.addReminderNotification(dbHandler.getListObject(idLo));
+			}
+
 		} else {
 
 		}
@@ -185,10 +254,10 @@ public class NewTaskActivity extends Activity {
 
 		/* Add items to the spinners */
 		/* Begin with the "TYPE" of reminder */
-		int imgArr[] = {R.drawable.device_access_alarms,
-				R.drawable.location_place};
-		String[] strTypeArr = {getString(R.string.time_reminder),
-				getString(R.string.location_reminder)};
+		int imgArr[] = { R.drawable.device_access_alarms,
+				R.drawable.location_place };
+		String[] strTypeArr = { getString(R.string.time_reminder),
+				getString(R.string.location_reminder) };
 
 		ReminderTypeAdapter typeAdapter = new ReminderTypeAdapter(this,
 				R.layout.type_spinner_item, strTypeArr, imgArr);
@@ -199,18 +268,22 @@ public class NewTaskActivity extends Activity {
 		/* Next up is the date of the reminder, simple enough */
 		ArrayAdapter<CharSequence> dateAdapter = ArrayAdapter
 				.createFromResource(this, R.array.test_dates,
-                        android.R.layout.simple_spinner_item);
+						android.R.layout.simple_spinner_item);
 		dateAdapter
 				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        Spinner dateSpinner = (Spinner) findViewById(R.id.reminderDateSpinner);
-        dateSpinner.setAdapter(dateAdapter);
+		Spinner dateSpinner = (Spinner) findViewById(R.id.reminderDateSpinner);
+		dateSpinner.setAdapter(dateAdapter);
 
-        /* Last but not least, time of the reminder with a clarification of the time */
-        String[] strTimeArr = {"Morning","Afternoon","Evening","Night"};
-        ReminderTimeAdapter timeAdapter = new ReminderTimeAdapter(this,
-                R.layout.time_spinner_item, strTimeArr);
-        Spinner timeSpinner = (Spinner) findViewById(R.id.reminderTimeSpinner);
-        timeSpinner.setAdapter(timeAdapter);
+		/*
+		 * Last but not least, time of the reminder with a clarification of the
+		 * time
+		 */
+		String[] strTimeArr = { "Morning", "Afternoon", "Evening", "Night" };
+		ReminderTimeAdapter timeAdapter = new ReminderTimeAdapter(this,
+				R.layout.time_spinner_item, strTimeArr);
+		Spinner timeSpinner = (Spinner) findViewById(R.id.reminderTimeSpinner);
+		timeSpinner.setAdapter(timeAdapter);
+		remindMeClicked = true;
 	}
 
 	public void onCancelReminder(View v) {
@@ -219,6 +292,7 @@ public class NewTaskActivity extends Activity {
 
 		currentLayout.setVisibility(View.INVISIBLE);
 		toBeShownLayout.setVisibility(View.VISIBLE);
+		remindMeClicked = false;
 	}
 
 	private void getTimeFromSpinners(String dateFromString, Date startDate,
