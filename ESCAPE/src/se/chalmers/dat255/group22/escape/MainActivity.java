acaffa1324d1c5be78cce27d589da51d4de9fcad
simonjrp <simon.jrp@gmail.com>
2013-10-04 12:13:30 +0200

    Back/up button now brings up the navigation drawer

--- ESCAPE/src/se/chalmers/dat255/group22/escape/MainActivity.java
+++ ESCAPE/src/se/chalmers/dat255/group22/escape/MainActivity.java
@@ -1,11 +1,14 @@
 package se.chalmers.dat255.group22.escape;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -17,18 +20,45 @@ public class MainActivity extends FragmentActivity {
 	private String[] drawerTitles;
 	private DrawerLayout drawerLayout;
 	private ListView drawerList;
-	
+	private ActionBarDrawerToggle drawerToggle;
+	private CharSequence appTitle;
+	private CharSequence drawerTitle;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		// Configure the navigation drawer
 		drawerTitles = getResources().getStringArray(R.array.drawer_titles);
 		drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 		drawerList = (ListView) findViewById(R.id.left_drawer);
-		
-		drawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, drawerTitles));
-		drawerList.setOnItemClickListener(new DrawerItemClickListener(this, drawerTitles, drawerLayout, drawerList));
-		
+
+		drawerList.setAdapter(new ArrayAdapter<String>(this,
+				R.layout.drawer_list_item, drawerTitles));
+		drawerList.setOnItemClickListener(new DrawerItemClickListener(this,
+				drawerTitles, drawerLayout, drawerList));
+		appTitle = drawerTitle = getTitle();
+
+		// Creates a new listener for drawer opened and closed events (to set
+		// title and hide icons in actionbar etc)
+		drawerToggle = new ActionBarDrawerToggle(this, drawerLayout,
+				R.drawable.ic_drawer, R.string.drawer_open,
+				R.string.drawer_close) {
+			public void onDrawerClosed(View drawerView) {
+				getActionBar().setTitle(appTitle);
+			}
+
+			public void onDrawerOpened(View view) {
+				getActionBar().setTitle(appTitle);
+				invalidateOptionsMenu();
+			}
+		};
+
+		drawerLayout.setDrawerListener(drawerToggle);
+
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		getActionBar().setHomeButtonEnabled(true);
 
 	}
 
@@ -41,7 +71,15 @@ public class MainActivity extends FragmentActivity {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
+		// Send item selected event to the drawer toggle (to handle click on the
+		// back/up button in the actionbar). If back/up button was pressed, the
+		// drawer toggle will return true, and false otherwised (i.e some other
+		// button in action bar was pressed)
+		if (drawerToggle.onOptionsItemSelected(item)) {
+			return true;
+		}
 
+		// Handle all action bar items except for the back/up button here.
 		switch (item.getItemId()) {
 		case R.id.add_task:
 			Intent intent = new Intent(this, NewTaskActivity.class);
@@ -50,6 +88,31 @@ public class MainActivity extends FragmentActivity {
 
 		return super.onOptionsItemSelected(item);
 	}
-	
-	
+
+	/*
+	 * Called when invalidateOptionsMenu() is called.
+	 * 
+	 * @see android.app.Activity#onPrepareOptionsMenu(android.view.Menu)
+	 */
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		// Hide all buttons in actionbar if navigation drawer is open
+		boolean drawerOpen = drawerLayout.isDrawerOpen(drawerList);
+		menu.findItem(R.id.add_task).setVisible(!drawerOpen);
+		return super.onPrepareOptionsMenu(menu);
+	}
+
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		// Sync the toggle state after onRestoreInstanceState has occurred.
+		drawerToggle.syncState();
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		drawerToggle.onConfigurationChanged(newConfig);
+	}
+
 }
