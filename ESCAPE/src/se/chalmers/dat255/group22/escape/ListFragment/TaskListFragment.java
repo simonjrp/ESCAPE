cc4063b87c29d30d485942eece1771a627f1f670
Carl Jansson <carjan91@gmail.com>
2013-10-04 12:16:56 +0200

    Made simple dummy popupMenu for task list category select

--- ESCAPE/src/se/chalmers/dat255/group22/escape/ListFragment/TaskListFragment.java
+++ ESCAPE/src/se/chalmers/dat255/group22/escape/ListFragment/TaskListFragment.java
@@ -1,6 +1,7 @@
 package se.chalmers.dat255.group22.escape.ListFragment;
 
 import se.chalmers.dat255.group22.escape.R;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,7 +11,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.PopupMenu;
+import android.widget.PopupWindow;
 
 /**
  * A fragment displaying a list with tasks. A task is different from an event
@@ -25,6 +27,8 @@ public class TaskListFragment extends Fragment {
 	// The adapter used to handle data
 	CustomListAdapter ourListAdapter;
 
+    PopupWindow popUp;
+
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
@@ -32,6 +36,7 @@ public class TaskListFragment extends Fragment {
         // Make fragment aware it has an action bar object!
         setHasOptionsMenu(true);
         initialize();
+        createPopup();
 	}
 
     @Override
@@ -44,8 +49,15 @@ public class TaskListFragment extends Fragment {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.pick_category :
-                Toast.makeText(getActivity(), "task category pick", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(getActivity(), "task category pick", Toast.LENGTH_SHORT).show();
                 //TODO implement a way to select categories to display
+                //popUp.showAsDropDown(getActivity().findViewById(R.id.pick_category));
+
+                View menuItemView = getActivity().findViewById(R.id.pick_category);
+                PopupMenu popup = new PopupMenu(getActivity(), menuItemView);
+                MenuInflater inflate = popup.getMenuInflater();
+                inflate.inflate(R.menu.popup_categories, popup.getMenu());
+                popup.show();
                 break;
         }
         return super.onOptionsItemSelected(item);
@@ -72,4 +84,14 @@ public class TaskListFragment extends Fragment {
 		ourTaskList = (ListView) getActivity().findViewById(R.id.listView);
 		ourTaskList.setAdapter(ourListAdapter);
 	}
+
+    private void createPopup(){
+        //LayoutInflater layoutInflater
+        //        = (LayoutInflater)getActivity()
+        //        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        //View popupView = layoutInflater.inflate(R.layout.popup_categories, null);
+        //popUp = new PopupWindow(popupView);
+        //popUp.setContentView(getActivity().findViewById(R.layout.popup_categories));
+        //popUp.setFocusable(true);
+    }
 }
